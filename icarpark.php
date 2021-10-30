cafecf825f6f15f51da9272fac5b909d6a2dc47e
billyng1994 <85629760+billyng1994@users.noreply.github.com>
2021-10-30 17:22:58 +0800

    Add files via upload

---
+++ icarpark.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>i-carpark</title>
+</head>
+<body>
+<form id="backtofit"  action="apigateway.php" method="GET"">
+    <select name="district" required="require" style="border-style: none; width:200px; height:25px">
+        <option>Mongkok</option>
+        <option>ShaTin</option>
+        <option>Taipo</option>
+    </select>
+    <button type="submit">submit</button>
+</form> 
+</body>
+</html>
+<?php
+
+
+
+?>
